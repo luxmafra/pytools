a28e6beb1e52c51e67c3a198f7d5f4809f333d08
luxmafra <luxmafra@gmail.com>
2020-09-29 15:40:11 +0100

    GitHub's API.

---
+++ pytools/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def buscar_avatar(usuario):
+    """
+    Search for an user's photo profile from Github
+
+    :param usuario: str
+    :return: str with the link of the photo profile
+    """
+    url = 'https://api.github.com/users/{}'.format(usuario)
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('luxmafra'))
