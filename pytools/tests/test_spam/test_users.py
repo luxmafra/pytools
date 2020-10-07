bb35c10d7356d978b8d0de577bc362c5013a935b
luxmafra <luxmafra@gmail.com>
2020-10-07 17:56:46 +0100

    Setup and Tear Down with Fixture.

--- pytools/tests/test_spam/test_users.py
+++ pytools/tests/test_spam/test_users.py
@@ -1,25 +1,36 @@
+import pytest
+
 from pytools.spam.db import Connect
 from pytools.spam.models import User
 
+@pytest.fixture
+def connect():
+    # Setup
+    connect_obj = Connect()
+    yield connect_obj
+    # Tear Down
+    connect_obj.close()
 
-def test_save_user():
-    connect = Connect()
-    session = connect.generate_session()
+@pytest.fixture
+def session(connect):
+    session_obj = connect.generate_session()
+    yield session_obj
+    session_obj.roll_back()
+    session_obj.close()
+
+
+def test_save_user(session):
     user = User(name='Lucas')
     session.save(user)
     assert isinstance(user.id, int)
-    session.roll_back()
-    session.close()
-    connect.close()
 
 
-def test_list_users():
-    connect = Connect()
-    session = connect.generate_session()
+
+def test_list_users(session):
     users = [User(name='Lucas'), User(name='Renzo')]
     for user in users:
         session.save(user)
     assert users == session.lst()
-    session.roll_back()
-    session.close()
-    connect.close()
+
+
+
