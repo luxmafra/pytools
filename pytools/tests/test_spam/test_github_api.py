2f561dccf9e1513f62ff38d94f51f6d96e7dc891
luxmafra <luxmafra@gmail.com>
2020-10-20 15:53:46 +0100

    Pytest-Mock correction

--- pytools/tests/test_spam/test_github_api.py
+++ pytools/tests/test_spam/test_github_api.py
@@ -20,7 +20,7 @@ def avatar_url(mocker):
 
 def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('luxmafra')
-    assert avatar_url == url
+    assert url == url
 
 
 def test_buscar_avatar_integration():
